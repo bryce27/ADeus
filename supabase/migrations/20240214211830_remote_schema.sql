8f81545e86fa054d8120607e07c1f9b7ebfc5b11
l33th4x0r <l33th4x0r@Blakes-MacBook-Pro.local>
2026-01-16 07:27:07 -0600

    chore: update package-lock and migrations

--- supabase/migrations/20240214211830_remote_schema.sql
+++ supabase/migrations/20240214211830_remote_schema.sql
@@ -9,6 +9,8 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+CREATE SCHEMA IF NOT EXISTS "pgsodium";
+
 CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
 
 CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
